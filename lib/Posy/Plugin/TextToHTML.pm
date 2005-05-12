a51039fad940b405b7c66c33195532b5a14033ed
Kathryn Andersen <rubykat@cpan.org>
2005-05-12 11:51:57 +0000

    Import of RUBYKAT/Posy-Plugin-TextToHTML-0.62 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-TextToHTML
    gitpan-cpan-version:      0.62
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-TextToHTML-0.62.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/TextToHTML.pm
+++ lib/Posy/Plugin/TextToHTML.pm
@@ -7,11 +7,11 @@ Posy::Plugin::TextToHTML - Posy plugin to convert plain text files to HTML.
 
 =head1 VERSION
 
-This describes version B<0.6101> of Posy::Plugin::TextToHTML.
+This describes version B<0.62> of Posy::Plugin::TextToHTML.
 
 =cut
 
-our $VERSION = '0.6101';
+our $VERSION = '0.62';
 
 =head1 SYNOPSIS
 
